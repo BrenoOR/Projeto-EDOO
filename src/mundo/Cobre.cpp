3194d7b29c7652e11c8a6f61d180d9fe81a321ed
Breno Rodrigues <bor@cin.ufpe.br>
2026-05-24 07:48:10 -0300

    fix: ores logging

--- src/mundo/Cobre.cpp
+++ src/mundo/Cobre.cpp
@@ -1,9 +1,11 @@
 #include "mundo/Cobre.h"
 #include "entidades/Personagem.h"
+#include <spdlog/spdlog.h>
 
 Cobre::Cobre(Rect b, TextureHandle tex) : Pedra(b, 1, tex) {}
 
 void Cobre::quebrar(Personagem& p) {
+    spdlog::debug("Cobre::quebrar dur={} morto={}", _durabilidade, _durabilidade <= 0);
     p.inventario.cobre += 1;
     _morto = true;
 }
--- src/mundo/Magnetita.cpp
+++ src/mundo/Magnetita.cpp
@@ -1,9 +1,11 @@
 #include "mundo/Magnetita.h"
 #include "entidades/Personagem.h"
+#include <spdlog/spdlog.h>
 
 Magnetita::Magnetita(Rect b, TextureHandle tex) : Pedra(b, 1, tex) {}
 
 void Magnetita::quebrar(Personagem& p) {
+    spdlog::debug("Magnetita::quebrar dur={} morto={}", _durabilidade, _durabilidade <= 0);
     p.inventario.magnetita += 1;
     p.danoPicareta = 1 + p.inventario.magnetita / 2;
     _morto = true;
